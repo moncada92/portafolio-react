33d2245ac595f4b8314c74b83bd31d51a473064d
Camilo Moncada <monk-da@hotmail.com>
2021-02-13 12:47:37 -0500

    add styled component to component AboutSection

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import AboutUs from './pages/AboutUs'
+// global styles
+import GlobalStyle from './components/GlobalStyle';
+
+//import page
+import AboutUs from './pages/AboutUs';
+
 
 const App = () => {
   return ( 
     <div className="app">
       <AboutUs />
+      <GlobalStyle />
     </div>
    );
 }
--- src/js/components/AboutSection.js
+++ src/js/components/AboutSection.js
@@ -1,31 +1,64 @@
 import React from 'react';
 import home1 from '../../img/home1.png'
+import styled from 'styled-components';
 
 const AboutSection = () => {
   return ( 
-    <div>
-      <div className="descriptio">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
-              your <span>dreamd</span> come
+              your <span>dreamd</span>
             </h2>
-          </div>
-          <div className="hide">
-            <h2>true.</h2>
-          </div>
+          </Hide>
+          <Hide>
+            <h2>come true.</h2>
+          </Hide>
           <p>Contact us for any photography or videography ideas that you have. We have professionals with amizing skills</p>
           <button>Contact us</button>
         </div>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="guy with a camer"/>
-      </div>
-    </div>
+      </Image>
+    </About>
    );
 }
  
+//style Compnents
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
 export default AboutSection;
