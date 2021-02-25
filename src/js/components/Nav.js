faad128f8b9295a30a5b5976747a18e7e4734eff
Camilo Moncada <monk-da@hotmail.com>
2021-02-24 23:12:56 -0500

    animation active link nav

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 const Nav = () => {
+
+  const { pathname } = useLocation();
   return ( 
     <StyledNav>
         <h1>
@@ -11,12 +15,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to="/">1. About Us</Link>
+          <Line 
+            transition = {{ duration: 0.75 }}
+            initial = {{ width: "0%" }}
+            animate = {{ width: pathname === "/" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/work">2. Our Work</Link>
+          <Line 
+            transition = {{ duration: 0.75 }}
+            initial = {{ width: "0%" }}
+            animate = {{ width: pathname === "/work" ? "50%" : "0%" }}
+          />
         </li> 
         <li>
           <Link to="/contact">3. Contact Us</Link>
+          <Line 
+            transition = {{ duration: 0.75 }}
+            initial = {{ width: "0%" }}
+            animate = {{ width: pathname === "/contact" ? "50%" : "0%" }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -66,8 +85,26 @@ const StyledNav = styled.nav`
       width: 100%;
     }
     li {
-      padding: 0rem 1.5rem;
+      padding: 0rem 0.5rem;
+    }
+  }
+
+  @media (max-width: 350px){
+    ul{
+      padding: 2rem 0.5rem;
     }
   }
 `
+
+const Line = styled(motion.div)`
+  height: 0.2rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1200px){
+    left: 0%;
+  }
+`;
 export default Nav;
