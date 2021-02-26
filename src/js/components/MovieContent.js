681b33fad1ba0d3d07edd1a0366d4aeefc392f64
Camilo Moncada <monk-da@hotmail.com>
2021-02-26 00:39:34 -0500

    config path movieContent 2

--- src/js/components/MovieContent.js
+++ src/js/components/MovieContent.js
@@ -37,6 +37,7 @@ const MovieContent = ({athlete, theracer, goodtimes}) => {
         <img src={theracer} alt="te racer"/>
       </Link>
     </Movie>
+    
     <Movie 
       ref={element2}
       variants={fade} 
